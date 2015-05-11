d555f31b2bda41061653bed0db91882f953e347c
thebrilliant <vivyanw@uw.edu>
2015-05-11 16:07:08 -0700

    added hash function

--- src/shake_n_bacon/StringHasher.java
+++ src/shake_n_bacon/StringHasher.java
@@ -3,19 +3,25 @@ package shake_n_bacon;
 import providedCode.Hasher;
 
 /**
- * @author <name>
- * @UWNetID <uw net id>
- * @studentID <id number>
- * @email <email address>
+ * @author Vivyan Woods, Fardos Mohamed
+ * @UWNetID vivyanw, ffm2
+ * @studentID 1327679, 1037290
+ * @email vivyanw@uw.edu, ffm2@uw.edu
  */
 public class StringHasher implements Hasher {
 
 	/**
-	 * TODO Replace this comment with your own as appropriate.
+	 * Given a string, breaks down the string into a number
+	 * based on the characters and their indexes.  Returns
+	 * the created number.
 	 */
 	@Override
 	public int hash(String str) {
-		// TODO Auto-generated method stub
-		return 0;
+		int result = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int temp = (int) str.charAt(i);
+			result += (temp * (Math.pow(37, i)));
+		}
+		return result;
 	}
 }
