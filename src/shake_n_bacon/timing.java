74bdeda1c052754971441cc4f0ba98ec569eadb8
Fardos Mohamed <ffm2@uw.edu>
2015-05-14 15:26:42 -0700

    timing.java added

---
+++ src/shake_n_bacon/timing.java
@@ -0,0 +1,39 @@
+/*
+ *@author Fardos Mohamed, Vivyan Woods
+ * @UWNetID ffm2, vivyanw
+ * @studentID 1037290, 1327679
+ * @email ffm2@uw.edu, vivyanw@uw.edu
+ * 
+ * 
+ * This is a timing file to test the different implementations and find the avg time for each implementation. 
+*/
+
+
+package shake_n_bacon;
+
+
+public class timing{
+	
+	private static final int NUM_TIMING= 10;
+	private static final int NUM_WARMUP = 5; //this will exclude the warmup 
+	private static final String TEXT = "Hamlet.txt";
+	
+	public static void main (String [] args){
+		String [] name = new String[] {"-o", TEXT};
+		System.out.println("run time: "+avgRunTime(args));
+	}
+	
+	private static double avgRunTime(String[] args){
+		double time = 0.0;
+		for (int i = 0; i < NUM_TIMING; i++){
+			long startTime = System.currentTimeMillis();
+			WordCount.main(args);
+			long endTime = System.currentTimeMillis();
+			if(NUM_WARMUP <= i){
+				time += (endTime - startTime);
+			}
+		}
+		return time / (NUM_TIMING - NUM_WARMUP);
+	}
+	
+}
